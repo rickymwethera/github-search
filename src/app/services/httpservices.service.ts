8a1ceba96f5fddb0b0bb27f94e32989d1dc8ae93
rickymwethera <rickymwethera@gmail.com>
2020-08-30 19:28:39 +0300

    added getprofile

--- src/app/services/httpservices.service.ts
+++ src/app/services/httpservices.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpservicesService {
+  private username:string;
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { 
+    this.username='rickymwethera'
+  }
+  getprofile(){
+    return this.http.get("https://api.github.com/users/" +this.username + "?access_token=" + environment.githubToken)
+
+  }
+  getrepos(){
+    return this.http.get("")
+  }
 }
