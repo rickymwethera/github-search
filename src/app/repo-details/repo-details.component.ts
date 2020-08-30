a99666dd9619bc911e2cead6a6dba2a8c1e405df
rickymwethera <rickymwethera@gmail.com>
2020-08-30 20:43:41 +0300

    added repo function

--- src/app/repo-details/repo-details.component.ts
+++ src/app/repo-details/repo-details.component.ts
@@ -8,7 +8,25 @@ import { HttpservicesService } from '../services/httpservices.service'
 })
 export class RepoDetailsComponent implements OnInit {
 
-  constructor() { }
+  profile:any[];
+  repos:any[];
+  username:string;
+
+  constructor(private httpService:HttpservicesService) { 
+    this.httpService.getProfile()
+  }
+  findProfile(){
+    this.httpService.updateProfile(this.username)
+    this.httpService.getProfile().subscribe(profile=>{
+      console.log(profile)
+      this.profile=profile
+    });
+    this.httpService.getrepos().subscribe(repo=>{
+      console.log(repo)
+      this.repos;
+    })
+  }
+
 
   ngOnInit(): void {
   }
