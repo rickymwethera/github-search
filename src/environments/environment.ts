4eda625a101045704eb2b20c6a429562dd024930
rickymwethera <rickymwethera@gmail.com>
2020-08-30 19:30:17 +0300

    added github API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  // githubToken:'57abb460160e85d95cf8a277c96fe75974163eb4';
+  githubToken:'57abb460160e85d95cf8a277c96fe75974163eb4'
 };
 
 /*
