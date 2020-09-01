9fd201140c63556d5d661ce9c8776186381ec5ad
rickymwethera <rickymwethera@gmail.com>
2020-09-02 00:02:02 +0300

    added routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RepoDetailsComponent } from './repo-details/repo-details.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {path: 'repo-details', component: RepoDetailsComponent},
+  {path: 'About', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpservicesService } from './services/httpservices.service';
-import { RepoDetailsComponent } from './repo-details/repo-details.component'
+import { RepoDetailsComponent } from './repo-details/repo-details.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component'
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepoDetailsComponent
+    RepoDetailsComponent,
+    NavBarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
